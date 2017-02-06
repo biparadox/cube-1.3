0d13f0a8dcc0d257070838f8270d8ed888a1df43
Hu jun <algorist@bjut.edu.cn>
2017-02-06 04:30:21 -0800

    remove struct_deal lib, instead it by struct_mod

--- cubelib/struct_deal/radix64.h
+++
@@ -1,9 +0,0 @@
-#ifndef RADIX_64_H
-#define RADIX_64_H
-
-int bin_to_radix64_len(int length);
-int radix_to_bin_len(int length);
-int bin_to_radix64(char * radix64,int length,unsigned char * bin); 
-int radix64_to_bin(unsigned char * bin,int length, char * radix64); 
-
-#endif
