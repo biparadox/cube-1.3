ebf8a9e21937871ea56e32c1e020c86383e8ff90
Hu jun <algorist@bjut.edu.cn>
2017-02-14 08:20:51 -0800

    remove file_dealer

--- proc/file_dealer/file_dealer.h
+++
@@ -1,7 +0,0 @@
-#ifndef FILE_DEALER_H
-#define FILE_DEALER_H
-
-int file_dealer_init(void * sub_proc,void * para);
-int file_dealer_start(void * sub_proc,void * para);
-
-#endif
--- set_env.sh
+++ set_env.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 export CUBE_PATH=`pwd`
 export CUBELIBPATH=$CUBE_PATH/cubelib
+export CUBESYSPATH=$CUBE_PATH/proc
 export LD_LIBRARY_PATH=$CUBELIBPATH/lib/:$LD_LIBRARY_PATH
 export CUBE_BASE_DEFINE=$CUBE_PATH/proc/main/base_define
 export CUBE_DEFINE_PATH=$CUBE_PATH/proc/define
