8d4b07c96125ee382304c06920077aa97cfe3bf6
Hu jun <algorist@bjut.edu.cn>
2017-10-23 01:05:35 -0700

    add sys_func's test_main

---
+++ cubelib/sys_func/test_main.c
@@ -0,0 +1,34 @@
+/**
+ * Copyright [2015] Tianfu Ma (matianfu@gmail.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * File: main.c
+ *
+ * Created on: Jun 5, 2015
+ * Author: Tianfu Ma (matianfu@gmail.com)
+ */
+
+#include <stdio.h>
+#include "../include/data_type.h"
+#include "../include/string.h"
+#include "../include/sys_func.h"
+
+int main() {
+
+	audit_file_init();
+	int i=10;
+	print_cubeerr("Test err no %d!\n",i);
+
+	return 0;
+}
