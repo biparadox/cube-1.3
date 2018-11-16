85c3df66e67ef123e9f459fae75798467ae5b302
Hu jun <algorist@bjut.edu.cn>
2018-11-16 01:08:07 -0800

    add msgfile_send module

---
+++ proc/src/msgfile_send/msgfile_send.c
@@ -0,0 +1,182 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <limits.h>
+#include <fcntl.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+ 
+#include "data_type.h"
+#include "alloc.h"
+#include "memfunc.h"
+#include "basefunc.h"
+#include "json.h"
+#include "struct_deal.h"
+#include "crypto_func.h"
+#include "memdb.h"
+#include "message.h"
+#include "ex_module.h"
+#include "sys_func.h"
+#include "msgfile_send.h"
+// add para lib_include
+
+int proc_msgfile_send(void * sub_proc,char * filename,void * recv_msg);
+int proc_basemsg(void * sub_proc,void * recv_msg);
+int msgfile_send_init(void * sub_proc, void * para)
+{
+	int ret;
+	// add yorself's module init func here
+	return 0;
+}
+int msgfile_send_start(void * sub_proc, void * para)
+{
+	int ret;
+	int i;
+	void * recv_msg;
+	struct start_para * start_para=para;
+	int type;
+	int subtype;
+	// add yorself's module exec func here
+	print_cubeaudit("begin msgfile_send module!\n");
+
+	if(para!=NULL)
+	{
+		if(start_para->argc>=2)
+		{
+			for(i=1;i<start_para->argc;i++)
+			{
+				ret=proc_msgfile_send(sub_proc,start_para->argv[i],NULL);
+				sleep(1);
+			}
+		}	
+	}	
+
+	while(1)
+	{
+		usleep(time_val.tv_usec);
+		ret=ex_module_recvmsg(sub_proc,&recv_msg);
+		if(ret<0)
+			continue;
+		if(recv_msg==NULL)
+			continue;
+		type=message_get_type(recv_msg);
+		subtype=message_get_subtype(recv_msg);
+		if(!memdb_find_recordtype(type,subtype))
+		{
+			printf("message format (%d %d) is not registered!\n",
+			message_get_type(recv_msg),message_get_subtype(recv_msg));
+			continue;
+		}
+		if((type==DTYPE_MESSAGE) &&(subtype==SUBTYPE_BASE_MSG))
+		{
+			proc_basemsg(sub_proc,recv_msg);
+		}
+	}
+	
+	return 0;
+}
+
+int proc_basemsg(void * sub_proc,void * recv_msg)
+{
+	int ret;
+	int i=0;
+
+	RECORD(MESSAGE,BASE_MSG) * base_msg;
+	
+	ret =message_get_record(recv_msg,&base_msg,i++);
+	if(ret<0)
+		return ret;
+	while(base_msg!=NULL)
+	{
+		proc_msgfile_send(sub_proc,base_msg->message,recv_msg);
+		ret=message_get_record(recv_msg,&base_msg,i++);	
+	}
+	return i;
+}
+
+int proc_msgfile_send(void * sub_proc, char * filename,void * recv_msg)
+{
+	int count=0;
+	int type,subtype;
+	void * head_node;
+	void * record_node;	
+	void * elem_node;	
+	int fd;
+	int ret;
+	void * record_template;
+	void * new_msg;
+	fd=open(filename,O_RDONLY);
+	if(fd<0)
+		return -EIO;
+	ret=read_json_node(fd,&head_node);
+	if(ret<0)
+		return -EINVAL;
+	if(head_node==NULL)
+		return -EINVAL;
+	elem_node=json_find_elem("type",head_node);
+	if(elem_node==NULL)
+	{
+		close(fd);
+		return -EINVAL;
+	}
+	type=memdb_get_typeno(json_get_valuestr(elem_node));
+	if(type<=0)
+	{
+		close(fd);
+		return -EINVAL;
+	} 			
+	elem_node=json_find_elem("subtype",head_node);
+	if(elem_node==NULL)
+	{
+		close(fd);
+		return -EINVAL;
+	}
+	subtype=memdb_get_subtypeno(type,json_get_valuestr(elem_node));
+	if(subtype<=0)
+	{
+		close(fd);
+		return -EINVAL;
+	} 
+
+	record_template=memdb_get_template(type,subtype);
+	if(record_template==NULL)
+	{
+		close(fd);
+		return -EINVAL;
+	}		
+	
+	ret=read_json_node(fd,&record_node);
+	int record_size=struct_size(record_template);	
+	void * record=NULL;
+
+	while(ret>0)
+	{
+		if(record==NULL)
+			record=Talloc(record_size);
+		if(record_node!=NULL)
+		{
+			ret=json_2_struct(record_node,record,record_template);
+			if(ret>=0)
+			{
+				new_msg=message_create(type,subtype,recv_msg);
+				if(new_msg==NULL)
+				{
+					printf("create message (%d %d) error!\n",type,subtype);
+					return -EINVAL;	
+				}	
+				message_add_record(new_msg,record);
+				ex_module_sendmsg(sub_proc,new_msg);
+				count++;
+			}
+		}	
+		ret=read_json_node(fd,&record_node);
+		if(record_node!=NULL)
+		{
+			sleep(1);
+		}
+	}
+	close(fd);
+	return count;
+}
---
+++ proc/src/msgfile_send/msgfile_send.h
@@ -0,0 +1,6 @@
+#ifndef MSGFILE_SEND_H
+#define MSGFILE_SEND_H
+ 
+int msgfile_send_init (void * sub_proc, void * para);
+int msgfile_send_start (void * sub_proc, void * para);
+#endif
