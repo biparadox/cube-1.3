7a9a7c9d5982130a089d1bc993204d37f412ddbd
Hu Jun <algorist@bjut.edu.cn>
2019-04-04 20:05:01 +0800

    trivial change in set_env.sh

--- set_env.sh
+++ set_env.sh
@@ -2,7 +2,7 @@
 export CUBE_PATH=`pwd`
 export CUBELIBPATH=$CUBE_PATH/cubelib
 export CUBESYSPATH=$CUBE_PATH/proc
-export LD_LIBRARY_PATH=$CUBELIBPATH/lib/:$CUBESYSPATH/plugin:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=$CUBELIBPATH/lib/:$LD_LIBRARY_PATH
 export CUBE_BASE_DEFINE=$CUBE_PATH/proc/main/base_define
 export CUBE_DEFINE_PATH=$CUBE_PATH/proc/define
 export CUBE_SYS_PLUGIN=$CUBE_PATH/proc/plugin/
