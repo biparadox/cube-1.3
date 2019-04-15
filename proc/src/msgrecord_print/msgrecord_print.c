ae57ef98add5f17deb9bde79631790eaf481edd0
Hu jun <algorist@bjut.edu.cn>
2019-04-14 23:21:54 -0700

    update with new code

--- proc/src/msgrecord_print/msgrecord_print.c
+++ proc/src/msgrecord_print/msgrecord_print.c
@@ -16,9 +16,6 @@
 #include "ex_module.h"
 #include "sys_func.h"
 #include "msgrecord_print.h"
-
-int proc_msgrecord_print(void * sub_proc,void * recv_msg);
-
 // add para lib_include
 int msgrecord_print_init(void * sub_proc, void * para)
 {
@@ -26,8 +23,6 @@ int msgrecord_print_init(void * sub_proc, void * para)
 	// add yorself's module init func here
 	return 0;
 }
-
-
 int msgrecord_print_start(void * sub_proc, void * para)
 {
 	int ret;
@@ -51,57 +46,6 @@ int msgrecord_print_start(void * sub_proc, void * para)
 			message_get_type(recv_msg),message_get_subtype(recv_msg));
 			continue;
 		}
-		proc_msgrecord_print(sub_proc,recv_msg);
 	}
 	return 0;
 }
-
-int proc_msgrecord_print(void * sub_proc,void * recv_msg)
-{
-	int type;
-	int subtype;
-	int i;
-	int ret;
-	char record_buf[DIGEST_SIZE*32];
-
-	type=message_get_type(recv_msg);
-	subtype=message_get_subtype(recv_msg);
-
-	void * record;
-
-	void * record_template;
-
-	i=0;
-
-	record_template=memdb_get_template(type,subtype);
-
-	if(record_template==NULL)
-	{
-		printf("record (%d %d) has no template!\n",type,subtype);
-		return 0;
-	}	
-
-	ret=message_get_record(recv_msg,&record,i++);
-	if(ret<0)
-		return ret;
-	while(record!=NULL)
-	{
-
-		ret=struct_2_json(record,record_buf,record_template);		
-		if(ret<0)
-		{
-			printf("record (%d %d) format error!\n",type,subtype);
-			return ret;
-		}
-		printf("%s\n",record_buf);
-		ret=message_get_record(recv_msg,&record,i++);
-		if(ret<0)
-			return ret;
-	}
-
-	ex_module_sendmsg(sub_proc,recv_msg);
-
-	return i;
-}
-
-
