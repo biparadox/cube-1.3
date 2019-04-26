7b869430d60a7106e9ba8772de105dcfb59a7daf
Hu Jun <algorist@bjut.edu.cn>
2019-04-26 17:49:12 +0800

    add msgtype_get plugin

---
+++ proc/src/msgtype_get/msgtype_get.h
@@ -0,0 +1,9 @@
+#ifndef MSGTYPE_GET_H
+#define MSGTYPE_GET_H
+
+
+// plugin's init func and kickstart func
+int msgtype_get_init(void * sub_proc,void * para);
+int msgtype_get_start(void * sub_proc,void * para);
+
+#endif
