6a01345c2078850938e7aa17ce87f6f3d82613fb
Hu Jun <algorist@bjut.edu.cn>
2019-11-27 04:57:34 -0800

    add running scripts in file_demo

---
+++ example/file_demo/file_trans2.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+rm file_sender/aspect_policy.cfg
+rm file_receiver/aspect_policy.cfg
+rm file_transfer/aspect_policy.cfg
+rm file_receiver/test.txt
+rm file_transfer/test.txt
+cp file_receiver/connector_config.cfg.transfer file_receiver/connector_config.cfg
+
+killall envset_proc
+sleep 1
+
+sh ../run_cube.sh exec_def/_file_receiver.def &
+sleep 1
+sh ../run_cube.sh exec_def/_file_transfer.def &
+sleep 1
+sh ../run_cube.sh exec_def/_file_sender.def request.txt &
