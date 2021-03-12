e333705be3741baae51ef41423f82ed82689e245
Hu jun <algorist@bjut.edu.cn>
2021-03-12 12:06:42 +0800

    change alicebob's setenv.sh

--- example/AliceBob/set_env.sh
+++ example/AliceBob/set_env.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 export CUBEAPPPATH=`pwd` 
 export CUBE_APP_PLUGIN=$CUBEAPPPATH/plugin/
-export CUBE_DEFINE_PATH=$CUBEAPPPATH/define/
+export CUBE_DEFINE_PATH=$CUBEAPPPATH/define/:$CUBE_DEFINE_PATH
