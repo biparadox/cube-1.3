ab9bfcaceb4b0409febe7162c0994d23674e94d8
Hu jun <algorist@bjut.edu.cn>
2022-07-07 20:25:45 +0800

    add test_main.c

---
+++ cubelib/crypto/test_main.c
@@ -0,0 +1,37 @@
+/**
+ * Copyright [2015] Tianfu Ma (matianfu@gmail.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * File: main.c
+ *
+ * Created on: Jun 5, 2015
+ * Author: Tianfu Ma (matianfu@gmail.com)
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "data_type.h"
+#include "list.h"
+#include "alloc.h"
+#include "memfunc.h"
+#include "basefunc.h"
+#include "struct_deal.h"
+#include "crypto_func.h"
+
+
+int main() {
+
+
+    return 0;
+}
