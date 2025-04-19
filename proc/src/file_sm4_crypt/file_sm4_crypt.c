8b6356dd150bcfa29ac6a0d95abadfceb64d4ed2
Hu jun <algorist@bjut.edu.cn>
2025-04-19 12:53:17 +0800

    add file_sm4_crypt plugin main func

--- proc/src/file_sm4_crypt/file_sm4_crypt.c
+++ proc/src/file_sm4_crypt/file_sm4_crypt.c
@@ -72,6 +72,8 @@ int proc_crypt_file(void *sub_proc, void *recv_msg)
 {
     void        *new_msg;
     char        file_buf[DIGEST_SIZE*4] = {0};
+    char        output_file_buf[DIGEST_SIZE*4] = {0};
+    int ret;
  
     int crypt_mode=0;   // 0: do nothing
                         // 1: crypt
@@ -87,7 +89,7 @@ int proc_crypt_file(void *sub_proc, void *recv_msg)
 	
     // get crypt cmd from message
     if ((ret = message_get_record(recv_msg, &cmd, 0)) < 0)
-        goto err;
+        return -EINVAL;
 
     // get command from message
 
@@ -97,11 +99,26 @@ int proc_crypt_file(void *sub_proc, void *recv_msg)
 
     Strncpy(file_buf,cmd->name+offset+1,DIGEST_SIZE*4);
        
-
-   if(Strncmp(file_buf,"crypt"==0))
+   // check mode and build output file name
+   if(Strncmp(cmd_buf,"encrypt",DIGEST_SIZE/2)==0)
+   {
 		crypt_mode=1;   
-   else if(Strncmp(file_buf,"decrypt"==0))
-		crypt_mode=2;   
+		Strncpy(output_file_buf,file_buf,DIGEST_SIZE*4-5);
+		Strcat(output_file_buf,".sm4");
+   }
+   else if(Strncmp(cmd_buf,"decrypt",DIGEST_SIZE/2)==0)
+   {
+		crypt_mode=2; 
+	        offset = Strnlen(file_buf,DIGEST_SIZE*4-1);
+		if(Strcmp(file_buf+offset-4,".sm4")!=0)
+		{
+			print_cubeerr("file_sm4_crypt: decrypt file name is not *.sm4 format!");
+			return -EINVAL;
+		}	
+		Strncpy(output_file_buf,file_buf,offset-4);
+   }
+
+   // get crypt key
 
    if(!Isemptyuuid(cmd->return_value))
    {
@@ -109,88 +126,27 @@ int proc_crypt_file(void *sub_proc, void *recv_msg)
    }
    else
    {
-	Memcpy(crypt_key,key,DIGEST_SIZE);
+	Memcpy(crypt_key,default_key,DIGEST_SIZE/2);
+	Memcpy(crypt_key+DIGEST_SIZE/2,default_iv,DIGEST_SIZE/2);
    }
 	
-
-    if ((file_key = Talloc0(sizeof(*file_key))) == NULL) {
-        ret = -ENOMEM;
-        goto err;
-    }
-    RAND_bytes(file_key->key,DIGEST_SIZE);
-    RAND_bytes(file_key->iv,DIGEST_SIZE);
-    file_key->crypt_start = 0;
-
-    if ((crypt_file = Talloc0(sizeof(*crypt_file))) == NULL) {
-        ret = -ENOMEM;
-        goto err;
-    }
-
-    /* Start encrypting files */
-    if ((ret = _datademo_file_sm4_crypt(source_file->file_name, crypt_file, file_key)) < 0)
-        flag = -1;
+    /* Start encrypt/decrypt  file */
+    if ((ret = _datademo_file_sm4_crypt(file_buf, output_file_buf,crypt_key,crypt_mode)) < 0)
+    	return -EINVAL;
     
-    if(source_file->user_name != NULL)
-    	Strncpy(crypt_file->user_name, source_file->user_name, DIGEST_SIZE);
-    Memcpy(crypt_file->src_uuid, node_info->node_uuid, DIGEST_SIZE);
+    if(crypt_mode)
+    {
+	    Free(cmd->name);
+	    cmd->name=dup_str(output_file_buf,DIGEST_SIZE*4);
+	    calculate_sm3(cmd->name,cmd->return_value);
+    }	
 
-    /* Store encrypted files */
-    db_record = memdb_store(crypt_file, TYPE_PAIR(DATADEMO_MAN,FILE), NULL);
-    if(db_record == NULL)
-	    printf("file_sm4_crypt: store crypt data failed \n");
-    /* store key */
-    db_record = memdb_store(file_key, TYPE_PAIR(DATADEMO_KEY,FILE_KEY), NULL);
-    if(db_record == NULL)
-	    printf("file_sm4_crypt: store file_key failed \n");
-    
-    // store the source file record    
-    void *types_msg = message_gen_typesmsg(TYPE_PAIR(DATADEMO_MAN,FILE),NULL);
-    if(types_msg == NULL) {
-        ret = -EINVAL;
-        goto err;
-    }
-	ex_module_sendmsg(sub_proc,types_msg);
-
-    void *types_msg1 = message_gen_typesmsg(TYPE_PAIR(DATADEMO_KEY,FILE_KEY),NULL);
-    if(types_msg1 == NULL) {
-        ret = -EINVAL;
-        goto err;
-    }
-    ex_module_sendmsg(sub_proc,types_msg1);
-
-    /* Return information after building file encryption */
-    if ((fileret = Talloc0(sizeof(*fileret))) == NULL){
-		ret = -ENOMEM;
-		goto err;
-	}
-
-	snprintf(buf, sizeof(buf), "The File action 'ACTION_REG' %s.", (flag == 0 ? "success" : "failed"));
-    fileret->info = strdup(buf);
-    fileret->action = qf->action;
-	fileret->state = (flag == 0 ? RET_SUCCESS : RET_FAILED);
-	strncpy(fileret->expand.uuid, crypt_file->uuid, sizeof(fileret->expand.uuid));
-	strncpy(fileret->expand.src_uuid, crypt_file->src_uuid, sizeof(fileret->expand.src_uuid));
-	strncpy(fileret->expand.user_name, crypt_file->user_name, sizeof(fileret->expand.user_name));
-	strncpy(fileret->expand.file_name, crypt_file->file_name, sizeof(fileret->expand.file_name));
-
-    new_msg = message_create(TYPE_PAIR(DATADEMO_QUERY,FILERETURN), recv_msg);
-	if(new_msg == NULL) {
-		ret = -EINVAL;
-		goto err;
-	}
-
-	if ((ret = message_add_record(new_msg, fileret)) < 0)
-		goto err;
-	
-	return ex_module_sendmsg(sub_proc, new_msg);
-err:
-    if (file_key) Free0(file_key);
-    if (crypt_file) Free0(crypt_file);
-    if (fileret) {
-        if (fileret->info) free(fileret->info);
-        Free0(fileret);
-    }
-    return ret;
+    new_msg = message_create(TYPE_PAIR(GENERAL_RETURN,UUID),recv_msg);
+    if(new_msg ==NULL)
+        return -EINVAL;
+    message_add_record(new_msg,cmd);
+  
+    return ex_module_sendmsg(sub_proc, new_msg);
 	
     /*i=0;
 
@@ -210,94 +166,20 @@ err:
 
     // get server info from share value
 
-/*
-    server_info = Talloc0(sizeof(*server_info));
-    if(server_info==NULL)
-        return -ENOMEM;
-
-    proc_share_data_getvalue("uuid",server_info->node_uuid);
-    proc_share_data_getvalue("proc_name",server_info->proc_name);
-    proc_share_data_getvalue("user_name",server_info->user_name);
-*/
     // create source file record 
 
     // generate file_key record
 
-   /* file_key = Talloc0(sizeof(*file_key));
-    if(file_key == NULL)
-        return -ENOMEM;
-
-    RAND_bytes(file_key->key,DIGEST_SIZE);
-    RAND_bytes(file_key->iv,DIGEST_SIZE);
-    file_key->crypt_start=0;
-
-    crypt_file = Talloc0(sizeof(*crypt_file));
-    if(crypt_file == NULL)
-        return -ENOMEM;
-
-    ret = _datademo_file_sm4_crypt(source_file->file_name,crypt_file,file_key);
-    if(ret<0)
-        return ret;
-    if(source_file->user_name !=NULL)
-    	Strncpy(crypt_file->user_name,source_file->user_name,DIGEST_SIZE);
-
-    Memcpy(crypt_file->src_uuid,node_info->node_uuid,DIGEST_SIZE);	
-
-    db_record = memdb_store(crypt_file,TYPE_PAIR(DATADEMO_MAN,FILE),NULL);
-    if(db_record ==NULL)
-	    printf("file_sm4_crypt: store crypt data failed \n");
-
-    db_record = memdb_store(file_key,TYPE_PAIR(DATADEMO_KEY,FILE_KEY),NULL);
-    if(db_record ==NULL)
-	    printf("file_sm4_crypt: store file_key failed \n");
-    // store the source file record    
-    void * types_msg =message_gen_typesmsg(TYPE_PAIR(DATADEMO_MAN,FILE),NULL);
-    if(types_msg==NULL)
-        return -EINVAL;
-	ex_module_sendmsg(sub_proc,types_msg);
-
-    void * types_msg1 =message_gen_typesmsg(TYPE_PAIR(DATADEMO_KEY,FILE_KEY),NULL);
-    if(types_msg1==NULL)
-        return -EINVAL;
-    ex_module_sendmsg(sub_proc,types_msg1);
-     
-    new_msg=message_create(TYPE_PAIR(DATADEMO_MAN,FILE),recv_msg);
-    message_add_record(new_msg,crypt_file);
-    ex_module_sendmsg(sub_proc,new_msg);
-
-    printf("file_sm4_crypt: send crpyt_file msg \n");
 
     return ret;*/
 }
 
-int proc_ctrl_message(void * sub_proc,void * message)
-{
-	int ret;
-	int i=0;
-	printf("begin proc echo \n");
-
-	struct ctrl_message * ctrl_msg;
-
-	
-	ret=message_get_record(message,&ctrl_msg,i++);
-	if(ret<0)
-		return ret;
-	if(ctrl_msg!=NULL)
-	{
-		ret=ctrl_msg->ctrl; 
-	}
-
-	ex_module_sendmsg(sub_proc,message);
-
-	return ret;
-}
-
-int _datademo_file_sm4_crypt(char * file_name, RECORD(DATADEMO_MAN,FILE) * crypt_file,
-    RECORD(DATADEMO_KEY,FILE_KEY) * file_key)
+int _datademo_file_sm4_crypt(char * file_name, char * output_file_name, BYTE * file_key,int crypt_mode) 
 {
     int ret;
     int fd;
     int target_fd;
+    int file_size;
     int block_size;
     int crypt_size;
     int total_crypt_size;
@@ -308,6 +190,9 @@ int _datademo_file_sm4_crypt(char * file_name, RECORD(DATADEMO_MAN,FILE) * crypt
     BYTE digest[DIGEST_SIZE];
     struct stat statbuf;
 
+    if(crypt_mode ==0)
+	   return  0;
+
     fd=open(file_name,O_RDONLY);
     if(fd<0)
         return fd;
@@ -316,24 +201,23 @@ int _datademo_file_sm4_crypt(char * file_name, RECORD(DATADEMO_MAN,FILE) * crypt
         print_cubeerr("fstat error\n");
         return -EINVAL;
     }
-    file_key->file_size=statbuf.st_size;
+    file_size=statbuf.st_size;
+
 
     block_size = DIGEST_SIZE*16;
 
     plain_buf = Buf;
     crypt_buf=plain_buf+block_size;
 
-    Strncpy(crypt_file->file_name,file_name,DIGEST_SIZE);
-    Strncat(crypt_file->file_name,".sec",DIGEST_SIZE);
-    target_fd=open(crypt_file->file_name,O_WRONLY|O_TRUNC|O_CREAT,0644);
+    target_fd=open(output_file_name,O_WRONLY|O_TRUNC|O_CREAT,0644);
     if(target_fd<0)
         return target_fd;
     
     offset=0;
     total_crypt_size=0;
-    while(offset<file_key->file_size)
+    while(offset<file_size)
     {
-        int left_size=file_key->file_size-offset;
+        int left_size=file_size-offset;
         if(left_size-offset > block_size)
         {
             ret = read(fd,plain_buf,block_size);
@@ -348,7 +232,7 @@ int _datademo_file_sm4_crypt(char * file_name, RECORD(DATADEMO_MAN,FILE) * crypt
                 return -EINVAL;
             offset+=left_size;
         }
-        crypt_size=sm4_context_crypt(plain_buf,&crypt_buf,ret,file_key->key);
+        crypt_size=sm4_context_crypt(plain_buf,&crypt_buf,ret,file_key);
         ret=write(target_fd,crypt_buf,crypt_size);
         if(ret!=crypt_size)
             return -EINVAL;
@@ -358,11 +242,5 @@ int _datademo_file_sm4_crypt(char * file_name, RECORD(DATADEMO_MAN,FILE) * crypt
     close(fd);
     close(target_fd);
     
-    calculate_sm3(crypt_file->file_name, crypt_file->uuid);
-    // digest_to_uuid(digest, crypt_file->uuid);
-
-    file_key->crypt_size=total_crypt_size; 
-    Memcpy(file_key->file_uuid,crypt_file->uuid,DIGEST_SIZE);
-
     return total_crypt_size;
 }
